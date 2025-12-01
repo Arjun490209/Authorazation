efa07d4d9133ce11d55b1ba6738c95ad64d3c06f
Arjun8400 <youji099@gmail.com>
2025-12-01 23:05:12 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -8,10 +8,31 @@ const app = express()
 
 const port = process.env.PORT || 5000
 app.use(express.json())
+
+// ! cors set
+const allowedOrigins = [
+  "http://localhost:5173",                  // Local dev
+  "https://authorazation-jbh6.vercel.app", // Production frontend
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}))
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true,
+// }))
+
+
+
 app.get('/',(req,res)=>{
     res.send("server run...")
 })
